e1251560629b30a66b22b705c3b2da8608f802a7
Anna Eiden <annaverenaeiden@hotmail.de>
2022-02-18 11:36:49 +0100

    Changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "lists", to: "lists#index"
-  get "lists/new", to: "lists#new", as: :new_list
-  post "lists", to: "lists#create"
-  get "lists/:id", to: "lists#show"
-
-  delete "/bookmarks/:id", to: "bookmarks#destroy"
-
-  resources :lists do
-    resources :bookmarks, only: [ :new, :create ]
+  root to: 'lists#index'
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+    resources :reviews, only: :create
   end
+  resources :bookmarks, only: :destroy
+  resources :reviews, only: :destroy
 end
